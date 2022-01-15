aaf2186b93f493992a39e531298d088ce7d14508
Kiranshejul <86283461+Kiranshejul@users.noreply.github.com>
2022-01-15 19:20:54 +0530

    Create Stringclass-2.java

---
+++ String Assignments/Stringclass-2.java
@@ -0,0 +1,12 @@
+public class string {
+
+	public static void main(String[] args) {
+	
+		String s1="Hello";
+		String s2="How are You!";
+		
+		String con=s1.concat(" "+ s2);
+		System.out.println(con);
+	}
+
+}

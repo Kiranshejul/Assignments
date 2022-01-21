99c787eb6453d5a6894af480f9f1eb451da48fc8
Kiranshejul <86283461+Kiranshejul@users.noreply.github.com>
2022-01-21 21:14:27 +0530

    Create 2_var.java

---
+++ 11 Feature Assignments/2_var.java
@@ -0,0 +1,17 @@
+public class VarProg {
+  
+	public static void main(String[] args) {
+
+		// Var can be declared any datatype
+        var p = 100;
+        var q = 1.90;
+        var r = 'a';
+        System.out.println(p);
+        System.out.println(q);
+        System.out.println(r);
+      
+        //cannot be used as a Generic type
+        
+	}
+
+}

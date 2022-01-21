5810856856ac0d73e93f4f66253e0d3c475a5ae9
Kiranshejul <86283461+Kiranshejul@users.noreply.github.com>
2022-01-21 21:51:53 +0530

    Create 4_student.java

---
+++ 11 Feature Assignments/4_student.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class StudentFile {
+  public static void main(String[] args) {
+
+	  int count=0;
+        Scanner file;
+        PrintWriter writer;
+        char ch;
+        
+        try {
+
+            file = new Scanner(new File("CC:\Users\Kiran Shejul\Desktop\capgemini/StudentList.txt")); //sourcefile
+            writer = new PrintWriter(C:\Users\Kiran Shejul\Desktop\capgemini/StudentList1.txt"); //destinationfile
+            
+            while (file.hasNext()) {
+                String line = file.nextLine();
+                if (!line.isEmpty()) {
+                    writer.write(line);
+                    writer.write("\n");
+                }
+            }
+
+            file.close();
+            writer.close();
+
+        } catch (FileNotFoundException ex) {
+            
+        }
+}
+}

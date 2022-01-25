dac8b426723cfd9d6c2fd565ad408ecc0c3eb0ff
Kiranshejul <86283461+Kiranshejul@users.noreply.github.com>
2022-01-25 15:32:18 +0530

    Update mainTest.java

--- Spring Assignment/SpringQ5/mainTest.java
+++ Spring Assignment/SpringQ5/mainTest.java
@@ -5,7 +5,7 @@ public class TestDemonstrate {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext con=new ClassPathXmlApplicationContext("Spring5Eg.xml");
+		ApplicationContext con=new ClassPathXmlApplicationContext("Spring5.xml");
 		
 		Demonstrate demo=(Demonstrate) con.getBean("msg1");
 		System.out.println(demo.getMsgID()+" "+demo.getMsg() );

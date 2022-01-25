922084ae07ae285c9dfcc85e5f3ef6f938ecebd5
Kiranshejul <86283461+Kiranshejul@users.noreply.github.com>
2022-01-25 15:29:56 +0530

    Create mainTest.java

---
+++ Spring Assignment/SpringQ5/mainTest.java
@@ -0,0 +1,14 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestDemonstrate {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext con=new ClassPathXmlApplicationContext("Spring5Eg.xml");
+		
+		Demonstrate demo=(Demonstrate) con.getBean("msg1");
+		System.out.println(demo.getMsgID()+" "+demo.getMsg() );
+		
+	}
+}
